de18deff69fad40e54de92b843cd25653721c856
Steve Mattsen <hugjuffs@gmail.com>
2021-03-14 00:27:22 -0600

    Set the wall's scale to exactly 1/14.

--- QuickDungeon.lua
+++ QuickDungeon.lua
@@ -177,7 +177,7 @@ function callbackSinglePlane(box, p1, p2)
     return nil
   end
   setSuperLock(box, true)
-  box.setScale({0.1, 0.2, p1:distance(p2) * 0.075})
+  box.setScale({0.1, 0.2, p1:distance(p2) * 0.0714286}) -- 1 / 14, width of walls.
   setSuperLock(box, true)
 end
 
